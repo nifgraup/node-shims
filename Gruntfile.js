f4255f0d82f6709b44f7c4964175142b7afebe72
Felix Hammerl <felix@whiteout.io>
2013-09-09 12:33:57 +0200

    add project configuration

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: ['*.js', '*/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/unit-test.js']
+            }
+        }
+    });
+
+    // Load the plugin(s)
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    // Default task(s).
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
+};
