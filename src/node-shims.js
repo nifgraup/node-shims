f28c6a3538caaff65fcc6ef567fe422138678276
Felix Hammerl <felix@whiteout.io>
2013-09-11 12:35:05 +0200

    move shims to source folder

---
+++ src/node-shims.js
@@ -0,0 +1,17 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function(require) {
+    'use strict';
+
+    var shims = {};
+
+    shims.util = require('./node-util');
+    shims.events = require('./node-events');
+    shims.Stream = require('./node-stream');
+    shims.net = require('./node-net');
+    shims.tls = require('./node-tls');
+    shims.Buffer = require('./node-buffer');
+    shims.crypto = require('./node-crypto');
+
+    return shims;
+});
